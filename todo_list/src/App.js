ce8045a51180f7c992ff86c60c494f9a25069e8b
ghildreth <gabrielantonhildreth@gmail.com>
2018-06-05 13:12:19 -0700

    add onChange, onSubmit functions

---
+++ todo_list/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+  onChange = (event) =>{
+    this.setState({term: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+        <input value={this.state.term} onChange={this.onChange} />
+        <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;

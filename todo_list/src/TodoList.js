a1c0f87594018fe3b8c4187bf02d56873c6bd91b
ghildreth <gabrielantonhildreth@gmail.com>
2018-06-05 15:07:26 -0700

    add flip move and delete functionality

--- todo_list/src/TodoList.js
+++ todo_list/src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
     constructor(props){
@@ -7,6 +9,7 @@ class TodoList extends Component {
             items: []
         };
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e){
@@ -30,7 +33,17 @@ class TodoList extends Component {
         e.preventDefault();
 
     }
-    
+
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item){
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+
     render(){
         return (
             <div className="todoListMain">
@@ -42,6 +55,8 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
+                <TodoItems entries={this.state.items}
+                delete={this.deleteItem}/>
             </div>
         );
     }
